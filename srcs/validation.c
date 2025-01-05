87e127eca635ebf2796b2d0c3b4ae03172cdecc7
kakra <kakra@student.42beirut.com>
2025-01-05 17:33:51 +0200

    parse cam

--- srcs/validation.c
+++ srcs/validation.c
@@ -21,16 +21,10 @@ int check_dup(char **line, t_val *val)
                 return (1);
             else if (!ft_strncmp(line[0], "L", ft_strlen(line[0])) && val->l == false)
                 val->l = true;
-            else if (!ft_strncmp(line[0], "cy", ft_strlen(line[0])) && val->cy == true)
-                return (1); 
             else if (!ft_strncmp(line[0], "cy", ft_strlen(line[0])) && val->cy == false)
                 val->cy = true;
-            else if (!ft_strncmp(line[0], "sp", ft_strlen(line[0])) && val->sp == true)
-                return (1);
             else if (!ft_strncmp(line[0], "sp", ft_strlen(line[0])) && val->sp == false)
                 val->sp = true;
-            else if (!ft_strncmp(line[0], "pl", ft_strlen(line[0])) && val->pl == true)
-                return (1);
             else if (!ft_strncmp(line[0], "pl", ft_strlen(line[0])) && val->pl == false)
                 val->pl = true;
         }
@@ -87,7 +81,9 @@ float string_to_float(const char *str) {
 int is_all_digits(const char *str)
 {
 	int i;
+    int f;
 
+    f = 0;
 	i = 0;
     if (!str || *str == '\0')
         return 0;
@@ -99,6 +95,12 @@ int is_all_digits(const char *str)
 			if (i > 0)
 				return (3);
 			i++;
+		}
+        else if (*str == '-')
+		{
+			if (f > 0)
+				return (3);
+			f++;
 		}
         else if (!ft_isdigit(*str))
             return 0;
@@ -119,7 +121,7 @@ int check_cor_a(char **line, t_cor *cor)
 	rgb = ft_split(line[2], ',');
 	if (twodsize(rgb) != 3)
 	{
-		printf("Error\nRGB Only Takes 3 Numbers\n");
+		printf("Error\nAmbient Light RGB Only Takes 3 Numbers\n");
 		freearray(rgb);
 		return (1);
 	}
@@ -131,7 +133,7 @@ int check_cor_a(char **line, t_cor *cor)
 	}
 	if (is_all_digits(rgb[0]) == 0 || is_all_digits(rgb[1]) == 0 || is_all_digits(rgb[2]) == 0 || is_all_digits(rgb[0]) == 3 || is_all_digits(rgb[1]) == 3 || is_all_digits(rgb[2]) == 3)
 	{
-		printf("Error\nRGB Should be a Number\n");
+		printf("Error\nAmbient Light RGB Should be a Number\n");
 		freearray(rgb);
 		return (1);
 	}
@@ -140,7 +142,7 @@ int check_cor_a(char **line, t_cor *cor)
 	cor->am.rgb[2] = string_to_float(rgb[2]);
 	if ((cor->am.rgb[0] > 255 || cor->am.rgb[0] < 0) || (cor->am.rgb[1] > 255 || cor->am.rgb[1] < 0) || (cor->am.rgb[2] > 255 || cor->am.rgb[2] < 0))
 	{
-		printf("Error\n RGB Range Is Between 0-255\n");
+		printf("Error\nAmbient Light RGB Range Is Between 0-255\n");
 		freearray(rgb);
 		return (1);
 	}
@@ -155,10 +157,88 @@ int check_cor_a(char **line, t_cor *cor)
 	return (0);
 }
 
+int check_cor_c(char **line , t_cor *cor)
+{
+    char **xyz;
+    char **vector;
+
+	if (twodsize(line) != 4)
+	{
+		printf("Error\nCamera only Takes 3 arguments\n");
+		return (1);
+	}
+	xyz = ft_split(line[1], ',');
+	vector = ft_split(line[2], ',');
+	if (twodsize(xyz) != 3)
+	{
+		printf("Error\nCamera Coordinates Of The Viewpoint Only Takes 3 Numbers\n");
+		freearray(xyz);
+		freearray(vector);
+		return (1);
+	}
+    if (twodsize(vector) != 3)
+	{
+		printf("Error\nCamera 3D Normalized Orientation Vector Only Takes 3 Numbers\n");
+		freearray(xyz);
+		freearray(vector);
+		return (1);
+	}
+	if (is_all_digits(line[3]) == 0 || is_all_digits(line[3]) == 3)
+	{
+		printf("Error\nCamera FOV Should be a Number\n");
+		freearray(xyz);
+		freearray(vector);
+		return (1);
+	}
+	if (is_all_digits(xyz[0]) == 0 || is_all_digits(xyz[1]) == 0 || is_all_digits(xyz[2]) == 0 || is_all_digits(xyz[0]) == 3 || is_all_digits(xyz[1]) == 3 || is_all_digits(xyz[2]) == 3)
+	{
+		printf("Error\nCamera Coordinates Of The Viewpoint Should be a Number\n");
+		freearray(xyz);
+		freearray(vector);
+		return (1);
+	}
+    if (is_all_digits(vector[0]) == 0 || is_all_digits(vector[1]) == 0 || is_all_digits(vector[2]) == 0 || is_all_digits(vector[0]) == 3 || is_all_digits(vector[1]) == 3 || is_all_digits(vector[2]) == 3)
+	{
+		printf("Error\nCamera 3D Normalized Orientation Vector Should be a Number\n");
+		freearray(xyz);
+		freearray(vector);
+		return (1);
+	}
+	cor->cm.cor[0] = string_to_float(xyz[0]);
+	cor->cm.cor[1] = string_to_float(xyz[1]);
+	cor->cm.cor[2] = string_to_float(xyz[2]);
+	cor->cm.vector[0] = string_to_float(vector[0]);
+	cor->cm.vector[1] = string_to_float(vector[1]);
+	cor->cm.vector[2] = string_to_float(vector[2]);
+    cor->cm.fov = string_to_float(line[3]);
+
+    if ((cor->cm.vector[0] > 1 || cor->cm.vector[0] < -1) || (cor->cm.vector[1] > 1 || cor->cm.vector[1] < -1) || (cor->cm.vector[2] > 1 || cor->cm.vector[2] < -1))
+	{
+		printf("Error\nCamera 3D Normalized Orientation Vector Range Is Between -1 -> 1\n");
+		freearray(xyz);
+		freearray(vector);
+		return (1);
+	}
+	if (cor->cm.fov > 180 || cor->cm.fov < 0)
+	{
+		printf("Error\nCamera Horizontal field of view Range Is Between 0 -> 180\n");
+		freearray(xyz);
+		freearray(vector);
+		return (1);
+	}
+	freearray(xyz);
+    freearray(vector);
+	return (0);
+}
+
 int	check_cor(char **line, t_cor *cor)
 {
 	if (line[0][0] == 'A')
 		check_cor_a(line, cor);
+    else if (line[0][0] == 'C')
+    {
+        check_cor_c(line, cor);
+    }
 	return (0);
 }
 t_val   *init_val(t_val *data)
